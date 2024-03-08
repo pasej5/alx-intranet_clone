a2a67a14cbc40c0a9417b5498813e50261cc2723
Marvin <marvinnamwamba@gmail.com>
2024-03-09 01:26:33 +0300

    added_dashboard.navigation.others

--- WebstackPortfolio/app/views.py
+++ WebstackPortfolio/app/views.py
@@ -2,5 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # A view No template.
+
+"""
+Context for the login page
+"""
 def login(request):
     return render(request, 'login.html')
+
+
+"""
+Context for the dashboard
+"""
+def dashboard(request):
+    return render(request, 'dashboard.html')
