31a110d1d1b110f3945835455ab0392a624ca4dc
Jealous Matsikachando <pasej5@gmail.com>
2024-03-09 09:01:03 +0200

    button alx log in

--- WebstackPortfolio/WebstackPortfolio/settings.py
+++ WebstackPortfolio/WebstackPortfolio/settings.py
@@ -1,3 +1,6 @@
+import os
+
+
 """
 Django settings for WebstackPortfolio project.
 
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
