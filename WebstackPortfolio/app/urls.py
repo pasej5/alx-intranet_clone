5d811cc5e3a2639d38f893b11e0c105b6a23c82d
Marvin <marvinnamwamba@gmail.com>
2024-03-23 17:07:03 +0300

    auto-clean

--- WebstackPortfolio/app/urls.py
+++ WebstackPortfolio/app/urls.py
@@ -8,12 +8,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('projects/', views.projects, name='projects'),
     path('concepts/', views.concepts, name='concepts'),
-<<<<<<< HEAD
     path('sandboxes/', views.sandboxes, name='sandboxes'),
     path('servers/', views.servers, name='servers'),
     path('tasks/<int:project_ID>', views.tasks, name='tasks'),
-=======
->>>>>>> 94667bdcf4133dcdd57d34616f0047a846a943b2
     path('homepage/', views.homepage, name='homepage'),
-    path('sandboxes', views.sandboxes, name='sandboxes')
 ]
