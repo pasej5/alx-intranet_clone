5e2b34f36d7d4fe93b7ebe0dfe1c5eec52a40a4c
Marvin <marvinnamwamba@gmail.com>
2024-03-29 22:50:14 +0300

    change allowed hosts to *

--- WebstackPortfolio/WebstackPortfolio/settings.py
+++ WebstackPortfolio/WebstackPortfolio/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8ps^2cj&725ov1pb2y42f)tx@q4=pq7lvw9#clhoc-aohvzhv1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
