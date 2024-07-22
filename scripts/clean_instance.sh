9e6e558361a90d4fe1b6e6fd80977cb97c2100eb
Jealous Matsikachando <pasej5@gmail.com>
2024-07-22 09:49:43 +0200

    clean

--- scripts/clean_instance.sh
+++ scripts/clean_instance.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
+# Stop the gunicorn and nginx services
+sudo service gunicorn stop
+sudo service nginx stop
+
+# Remove previous application files
 sudo rm -rf /home/ubuntu/WebstackPortfolio/*
