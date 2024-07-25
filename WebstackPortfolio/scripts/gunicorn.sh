42a80f4ec61faaf53d0089b2261fda4a94975f7c
Jealous Matsikachando <pasej5@gmail.com>
2024-07-25 04:55:30 +0200

    chomd

--- WebstackPortfolio/scripts/gunicorn.sh
+++ WebstackPortfolio/scripts/gunicorn.sh
@@ -4,11 +4,13 @@
 PROJECT_DIR="/home/ubuntu/alx-intranet_clone/WebstackPortfolio"
 GUNICORN_SOCKET="/run/gunicorn.sock"
 GUNICORN_BIN="$PROJECT_DIR/new_env/bin/gunicorn"
-APP_MODULE="WebstackPortfolio.wsgi:application"
+APP_MODULE="WebstackPortfolio.WebstackPortfolio.wsgi:application"
 
 # Create the /run directory if it doesn't exist and set permissions
 sudo mkdir -p /run
 sudo chown ubuntu:ubuntu /run
+sudo chown -R ubuntu:ubuntu $PROJECT_DIR
+
 
 # Navigate to project directory
 cd $PROJECT_DIR
