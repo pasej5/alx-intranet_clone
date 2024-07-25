7ffa80e7750563d900d50de3824e5fc4bcf9bc5e
Jealous Matsikachando <pasej5@gmail.com>
2024-07-25 05:41:47 +0200

    new requirements

--- WebstackPortfolio/scripts/gunicorn.sh
+++ WebstackPortfolio/scripts/gunicorn.sh
@@ -2,9 +2,11 @@
 
 # Define variables
 PROJECT_DIR="/home/ubuntu/alx-intranet_clone/WebstackPortfolio/WebstackPortfolio"
+VENV_DIR="$PROJECT_DIR/new_env"
 GUNICORN_SOCKET="/run/gunicorn.sock"
-GUNICORN_BIN="$PROJECT_DIR/WebstackPortfolio/new_env/bin/gunicorn"
+GUNICORN_BIN="$VENV_DIR/bin/gunicorn"
 APP_MODULE="WebstackPortfolio.wsgi:application"
+REQUIREMENTS_FILE="$PROJECT_DIR/requirements.txt"
 
 # Create the /run directory if it doesn't exist and set permissions
 sudo mkdir -p /run
@@ -14,25 +16,28 @@ sudo chown ubuntu:ubuntu /run
 sudo chown -R ubuntu:ubuntu $PROJECT_DIR
 
 # Recreate the virtual environment if needed
-if [ ! -d "$PROJECT_DIR/WebstackPortfolio/WebstackPortfolio/new_env" ]; then
+if [ ! -d "$VENV_DIR" ]; then
     echo "Virtual environment not found. Creating a new one..."
-    python3 -m venv $PROJECT_DIR/WebstackPortfolio/WebstackPortfolio/new_env
+    python3 -m venv $VENV_DIR
 fi
 
 # Activate the virtual environment and install dependencies
-source $PROJECT_DIR/WebstackPortfolio/WebstackPortfolio/new_env/bin/activate
+source $VENV_DIR/bin/activate
 pip install --upgrade pip
-pip install -r $PROJECT_DIR/requirements.txt
-chmod 644 /home/ubuntu/alx-intranet_clone/WebstackPortfolio/WebstackPortfolio/requirements.txt
+if [ -f "$REQUIREMENTS_FILE" ]; then
+    pip install -r $REQUIREMENTS_FILE
+else
+    echo "Requirements file not found at $REQUIREMENTS_FILE"
+    exit 1
+fi
 
 # Fix shebang line in gunicorn if needed
-GUNICORN_EXECUTABLE="$PROJECT_DIR/WebstackPortfolio/WebstackPortfolio/new_env/bin/gunicorn"
-if [ -f "$GUNICORN_EXECUTABLE" ]; then
-    CURRENT_SHEBANG=$(head -n 1 "$GUNICORN_EXECUTABLE")
-    EXPECTED_SHEBANG="#!/home/ubuntu/alx-intranet_clone/WebstackPortfolio/WebstackPortfolio/new_env/bin/python3"
+if [ -f "$GUNICORN_BIN" ]; then
+    CURRENT_SHEBANG=$(head -n 1 "$GUNICORN_BIN")
+    EXPECTED_SHEBANG="#!/home/ubuntu/alx-intranet_clone/WebstackPortfolio/new_env/bin/python3"
     if [ "$CURRENT_SHEBANG" != "$EXPECTED_SHEBANG" ]; then
         echo "Updating shebang line in gunicorn executable..."
-        sed -i "1s|$CURRENT_SHEBANG|$EXPECTED_SHEBANG|" "$GUNICORN_EXECUTABLE"
+        sed -i "1s|$CURRENT_SHEBANG|$EXPECTED_SHEBANG|" "$GUNICORN_BIN"
     fi
 else
     echo "Gunicorn executable not found. Please check your setup."
