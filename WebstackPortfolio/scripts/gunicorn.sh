128861e5dd72e331ccd2af762e0da823d68efacf
Jealous Matsikachando <pasej5@gmail.com>
2024-07-25 05:53:55 +0200

    requirements debug

--- WebstackPortfolio/scripts/gunicorn.sh
+++ WebstackPortfolio/scripts/gunicorn.sh
@@ -6,7 +6,7 @@ VENV_DIR="$PROJECT_DIR/new_env"
 GUNICORN_SOCKET="/run/gunicorn.sock"
 GUNICORN_BIN="$VENV_DIR/bin/gunicorn"
 APP_MODULE="WebstackPortfolio.wsgi:application"
-REQUIREMENTS_FILE="$PROJECT_DIR/requirements.txt"
+REQUIREMENTS_FILE="/home/ubuntu/alx-intranet_clone/WebstackPortfolio/requirements.txt"
 
 # Create the /run directory if it doesn't exist and set permissions
 sudo mkdir -p /run
@@ -34,7 +34,7 @@ fi
 # Fix shebang line in gunicorn if needed
 if [ -f "$GUNICORN_BIN" ]; then
     CURRENT_SHEBANG=$(head -n 1 "$GUNICORN_BIN")
-    EXPECTED_SHEBANG="#!/home/ubuntu/alx-intranet_clone/WebstackPortfolio/new_env/bin/python3"
+    EXPECTED_SHEBANG="#!/home/ubuntu/alx-intranet_clone/WebstackPortfolio/WebstackPortfolio/new_env/bin/python3"
     if [ "$CURRENT_SHEBANG" != "$EXPECTED_SHEBANG" ]; then
         echo "Updating shebang line in gunicorn executable..."
         sed -i "1s|$CURRENT_SHEBANG|$EXPECTED_SHEBANG|" "$GUNICORN_BIN"
