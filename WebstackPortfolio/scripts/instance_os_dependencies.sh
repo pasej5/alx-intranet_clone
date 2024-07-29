c87df95223946d77cdef194aed94e9aab47249c6
Jealous Matsikachando <pasej5@gmail.com>
2024-07-29 11:21:17 +0200

    new file structure

--- WebstackPortfolio/scripts/instance_os_dependencies.sh
+++ WebstackPortfolio/scripts/instance_os_dependencies.sh
@@ -3,6 +3,10 @@
 # Exit on any error
 set -e
 
+# Update and install system dependencies
+sudo apt-get update
+sudo apt-get install -y nginx python3-pip python3-venv
+
 # Create the virtual environment if it doesn't exist
 if [ ! -d "/home/ubuntu/alx-intranet_clone/WebstackPortfolio/new_env" ]; then
     python3 -m venv /home/ubuntu/alx-intranet_clone/WebstackPortfolio/new_env
@@ -11,10 +15,10 @@ fi
 # Activate the virtual environment
 source /home/ubuntu/alx-intranet_clone/WebstackPortfolio/new_env/bin/activate
 
-# Install system dependencies
-sudo apt-get update
-sudo apt-get install -y nginx
-
-# Install Python dependencies
+# Upgrade pip and install Python dependencies
 pip install --upgrade pip
 pip install -r /home/ubuntu/alx-intranet_clone/WebstackPortfolio/requirements.txt
+
+# Create necessary directories for logs if they do not exist
+mkdir -p /home/ubuntu/alx-intranet_clone/WebstackPortfolio/logs
+sudo chown -R ubuntu:ubuntu /home/ubuntu/alx-intranet_clone/WebstackPortfolio/logs
